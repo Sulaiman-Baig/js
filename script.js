c98f120e2cda51b00b415091bd97470ce0e9825a
Azeem-pp <Mazeem009900@gmail.com>
2023-10-04 11:49:15 +0500

    array-basics

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 
 
 
-// alert(1);
+// // alert(1);
 
 
-// This is a single line of code
+// // This is a single line of code
 
-/*
-multi-line comment
-console.log(100);
-console.log('Hello World');
-console.log(20, 'hello', true);
-*/
+// /*
+// multi-line comment
+// console.log(100);
+// console.log('Hello World');
+// console.log(20, 'hello', true);
+// */
 
-const x = 100;
-console.log(x);
+// const x = 100;
+// console.log(x);
 
 
-// try {
+// // try {
     
-// } catch (error) {
+// // } catch (error) {
     
-// }
-// if (condition) {
-// }
+// // }
+// // if (condition) {
+// // }
 
-// console.edit('x');
+// // console.edit('x');
+// // console.error('Alert');
+// console.warn('Warnning');
+
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+// console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
+
+// console.group('simple');
+// console.log(x);
 // console.error('Alert');
-console.warn('Warnning');
-
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-console.table({name: 'Azeem', email: 'Azeem@gmail.com'});
-
-console.group('simple');
-console.log(x);
-console.error('Alert');
-console.warn('Warnning');
-console.groupEnd();
+// console.warn('Warnning');
+// console.groupEnd();
 
 
 
-const styles = 'padding: 10px; background-color: white; color: green';
-console.log('%cHello World', styles);
+// const styles = 'padding: 10px; background-color: white; color: green';
+// console.log('%cHello World', styles);
