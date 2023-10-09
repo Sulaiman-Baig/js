a3ea4e2336a9aa714e892e6f572da64978f6b3d4
Azeem-pp <Mazeem009900@gmail.com>
2023-10-09 21:26:38 +0500

    Calculator-Challenge

---
+++ 20-Calculator-Challenge/script.js
@@ -0,0 +1,25 @@
+function Calculator(num1, num2, operator) {
+  let result;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      result = 'Invalid Operator';
+  }
+
+  console.log(result);
+  return result;
+}
+
+Calculator(10, 6, '-');
