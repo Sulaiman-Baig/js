f7ab425b9fbfc03309f45df02c7709b1a6312d82
Azeem-pp <Mazeem009900@gmail.com>
2023-10-25 16:54:15 +0500

    Event-Delegation-Multiple-Events

---
+++ 49-Event-Delegation-Multiple-Events/script.js
@@ -0,0 +1,22 @@
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+listItems.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    e.target.remove();
+  });
+});
+
+list.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
+});
+
+list.addEventListener('mouseover', (e) => {
+  console.log(e.target);
+  if (e.target.tagName === 'LI') {
+    e.target.style.color = 'red';
+  }
+});
