2c7533ec9bae151db832aed99d285fd7284db3a1
Azeem-pp <Mazeem009900@gmail.com>
2023-10-26 16:24:05 +0500

    Async

---
+++ 01-Async/script.js
@@ -0,0 +1,23 @@
+// Asynchronous
+
+console.log('I');
+console.log('eat');
+
+setTimeout(() => {
+  console.log('Ice-cream');
+}, 3000);
+
+console.log('with a');
+console.log('spoon');
+
+function one(call_two) {
+  console.log('Step 1 complete. Please Call Step 2');
+  call_two();
+}
+
+function two(call_one) {
+  console.log('step 2 ');
+}
+
+// two();
+one(two);
