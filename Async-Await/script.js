49e0a60f0a3635e5504f6dc2c408883ebec7106e
Azeem-pp <Mazeem009900@gmail.com>
2023-10-27 18:21:40 +0500

    Async-Await

---
+++ Async-Await/script.js
@@ -0,0 +1,18 @@
+let stocks = {
+  Fruits: ['strawberry', 'grapes', 'bananana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts'],
+};
+
+let is_shop_open = true;
+
+let order = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+};
