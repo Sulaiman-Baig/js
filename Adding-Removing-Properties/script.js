3b944a612d75fada20d89b810c72072e9a587ab6
Azeem-pp <Mazeem009900@gmail.com>
2023-10-30 16:33:09 +0500

    Adding-Removing-Properties

---
+++ Adding-Removing-Properties/script.js
@@ -0,0 +1,22 @@
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log('draw');
+  };
+}
+
+const circle = new Circle(10);
+
+circle.location = { x: 1 };
+// User.token = 'asdfasdf';
+
+circle.location = { x: 1 };
+const properyName = 'location';
+circle.location;
+
+// circle['location'] = { x: 1 };
+
+circle['propertyName'] = 'center location';
+circle['propertyName'] = { x: 1 };
+
+delete circle['location'];
